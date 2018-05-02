a3ef3e787ccc0e3edc3ad1c10b2669f9c9f52313
Chad Darby <cdarby@redhat.com>
2018-05-02 08:21:32 -0700

    lab 1 - commented out extra test cases

--- lab-01/src/test/java/com/redhat/coolstore/cart/service/ShoppingCartServiceImplTest.java
+++ lab-01/src/test/java/com/redhat/coolstore/cart/service/ShoppingCartServiceImplTest.java
@@ -66,7 +66,7 @@ public class ShoppingCartServiceImplTest {
         }).when(priceCalculationService).priceShoppingCart(any(ShoppingCart.class));
     }
     
-    @Test
+    // @Test
     public void testGetNewShoppingCart() {
         ShoppingCart sc = shoppingCartService.getShoppingCart("123456");
 
@@ -78,7 +78,7 @@ public class ShoppingCartServiceImplTest {
         assertThat(sc.getShoppingCartItemList().size(), equalTo(0));
     }
 
-    @Test
+    // @Test
     public void testAddNewItemToCart() {
 
         ShoppingCart sc = shoppingCartService.addToCart("123456", "p1", 1);
@@ -98,153 +98,5 @@ public class ShoppingCartServiceImplTest {
         assertThat(sc.getShoppingCartItemList().size(), equalTo(1));
         assertThat(sc.getCartItemTotal(), equalTo(100.0));
     }
-
-    @Test
-    public void testAddExistingItemToCart() {
-
-        shoppingCartService.addToCart("123456", "p1", 1);
-        ShoppingCart sc = shoppingCartService.addToCart("123456", "p1", 2);
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(1));
-        ShoppingCartItem sci = sc.getShoppingCartItemList().get(0);
-        assertThat(sci.getProduct(), notNullValue());
-        assertThat(sci.getProduct().getItemId(), equalTo("p1"));
-        assertThat(sci.getProduct().getPrice(), equalTo(100.0));
-        assertThat(sci.getPrice(), equalTo(100.0));
-        assertThat(sci.getQuantity(), equalTo(3));
-        verify(priceCalculationService, times(2)).priceShoppingCart(any(ShoppingCart.class));
-        verify(catalogService, times(2)).getProduct("p1");
-
-        //make sure the cart store is up to date
-        sc = shoppingCartService.getShoppingCart("123456");
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(1));
-        assertThat(sc.getShoppingCartItemList().get(0).getQuantity(), equalTo(3));
-    }
-
-    @Test
-    public void testAddItemToCartWithNegativeOrNullQuantity() {
-
-        shoppingCartService.addToCart("123456", "p1", 1);
-        ShoppingCart sc = shoppingCartService.addToCart("123456", "p1", 0);
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(1));
-        ShoppingCartItem sci = sc.getShoppingCartItemList().get(0);
-        assertThat(sci.getProduct(), notNullValue());
-        assertThat(sci.getProduct().getItemId(), equalTo("p1"));
-        assertThat(sci.getProduct().getPrice(), equalTo(100.0));
-        assertThat(sci.getPrice(), equalTo(100.0));
-        assertThat(sci.getQuantity(), equalTo(1));
-        verify(priceCalculationService).priceShoppingCart(any(ShoppingCart.class));
-        verify(catalogService).getProduct("p1");
-
-        ShoppingCart sc1 = shoppingCartService.addToCart("123456", "p1", -1);
-        assertThat(sc1.getShoppingCartItemList().size(), equalTo(1));
-        ShoppingCartItem sci1 = sc1.getShoppingCartItemList().get(0);
-        assertThat(sci1.getQuantity(), equalTo(1));
-        verify(priceCalculationService).priceShoppingCart(any(ShoppingCart.class));
-        verify(catalogService).getProduct("p1");
-    }
-
-    @Test
-    public void testAddItemToCartWithOtherProducts() {
-
-        shoppingCartService.addToCart("123456", "p1", 1);
-        ShoppingCart sc = shoppingCartService.addToCart("123456", "p2", 2);
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(2));
-        Optional<ShoppingCartItem> sci = sc.getShoppingCartItemList().stream().filter(i -> i.getProduct().getItemId().equals("p2")).findFirst();
-        assertThat(sci.isPresent(), equalTo(true));
-        assertThat(sci.get().getQuantity(), equalTo(2));
-        verify(priceCalculationService, times(2)).priceShoppingCart(any(ShoppingCart.class));
-        verify(catalogService).getProduct("p1");
-        verify(catalogService).getProduct("p2");
-
-        //make sure the cart store is up to date
-        sc = shoppingCartService.getShoppingCart("123456");
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(2));
-    }
-
-    @Test
-    public void testAddNonExistingItemToCartWithOtherProducts() {
-
-        shoppingCartService.addToCart("123456", "p1", 1);
-        ShoppingCart sc = shoppingCartService.addToCart("123456", "p3", 2);
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(1));
-        verify(priceCalculationService, times(1)).priceShoppingCart(any(ShoppingCart.class));
-        verify(catalogService).getProduct("p1");
-        verify(catalogService).getProduct("p3");
-    }
-
-    @Test
-    public void testRemoveExistingItemFromCartWithNegativeOrNullQuantity() {
-
-        shoppingCartService.addToCart("123456", "p1", 3);
-        ShoppingCart sc = shoppingCartService.removeFromCart("123456", "p1", 0);
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(1));
-        ShoppingCartItem sci = sc.getShoppingCartItemList().get(0);
-        assertThat(sci.getProduct(), notNullValue());
-        assertThat(sci.getProduct().getItemId(), equalTo("p1"));
-        assertThat(sci.getProduct().getPrice(), equalTo(100.0));
-        assertThat(sci.getQuantity(), equalTo(3));
-        verify(priceCalculationService).priceShoppingCart(any(ShoppingCart.class));
-
-        ShoppingCart sc1 = shoppingCartService.removeFromCart("123456", "p1", -2);
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(1));
-        ShoppingCartItem sci1 = sc1.getShoppingCartItemList().get(0);
-        assertThat(sci1.getQuantity(), equalTo(3));
-        verify(priceCalculationService).priceShoppingCart(any(ShoppingCart.class));
-    }
-
-    @Test
-    public void testRemoveExistingItemFromCart() {
-
-        shoppingCartService.addToCart("123456", "p1", 3);
-        ShoppingCart sc = shoppingCartService.removeFromCart("123456", "p1", 2);
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(1));
-        ShoppingCartItem sci = sc.getShoppingCartItemList().get(0);
-        assertThat(sci.getProduct(), notNullValue());
-        assertThat(sci.getProduct().getItemId(), equalTo("p1"));
-        assertThat(sci.getProduct().getPrice(), equalTo(100.0));
-        assertThat(sci.getPrice(), equalTo(100.0));
-        assertThat(sci.getQuantity(), equalTo(1));
-        verify(priceCalculationService, times(2)).priceShoppingCart(any(ShoppingCart.class));
-        verify(catalogService).getProduct("p1");
-
-        //make sure the cart store is up to date
-        sc = shoppingCartService.getShoppingCart("123456");
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(1));
-    }
-
-    @Test
-    public void testRemoveAllItemsFromCart() {
-        shoppingCartService.addToCart("123456", "p1", 3);
-        ShoppingCart sc = shoppingCartService.removeFromCart("123456", "p1", 3);
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(0));
-        verify(priceCalculationService, times(2)).priceShoppingCart(any(ShoppingCart.class));
-        verify(catalogService).getProduct("p1");
-
-        //make sure the cart store is up to date
-        sc = shoppingCartService.getShoppingCart("123456");
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(0));
-    }
-
-    @Test
-    public void testCheckoutCart() {
-        shoppingCartService.addToCart("123456", "p1", 3);
-        ShoppingCart sc = shoppingCartService.checkoutShoppingCart("123456");
-        assertThat(sc, notNullValue());
-        assertThat(sc.getId(), equalTo("123456"));
-        assertThat(sc.getCartItemTotal(), equalTo(0.0));
-        assertThat(sc.getShippingTotal(), equalTo(0.0));
-        assertThat(sc.getCartTotal(), equalTo(0.0));
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(0));
-
-        //make sure we also updated the cart store
-        sc = shoppingCartService.getShoppingCart("123456");
-        assertThat(sc, notNullValue());
-        assertThat(sc.getId(), equalTo("123456"));
-        assertThat(sc.getCartItemTotal(), equalTo(0.0));
-        assertThat(sc.getShippingTotal(), equalTo(0.0));
-        assertThat(sc.getCartTotal(), equalTo(0.0));
-        assertThat(sc.getShoppingCartItemList().size(), equalTo(0));
-    }
-
 
 }
