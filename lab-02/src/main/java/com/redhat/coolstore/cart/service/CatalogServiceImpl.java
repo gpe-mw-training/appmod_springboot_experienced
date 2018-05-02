7410dcc43c6cb2c03ab9eb0d324a3353076a1f6a
Chad Darby <cdarby@redhat.com>
2018-05-02 07:00:40 -0700

    base files

---
+++ lab-02/src/main/java/com/redhat/coolstore/cart/service/CatalogServiceImpl.java
@@ -0,0 +1,15 @@
+package com.redhat.coolstore.cart.service;
+
+import com.redhat.coolstore.cart.model.Product;
+
+public class CatalogServiceImpl implements CatalogService {
+
+	@Override
+	public Product getProduct(String itemId) {
+
+		// TODO: Replace this method
+		
+		return null;
+	}
+
+}
