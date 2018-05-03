3fa29af972fa5e3ad38e11af52dc77e643e55d61
Chad Darby <cdarby@redhat.com>
2018-05-03 04:37:46 -0700

    removed unused code

--- lab-02/src/test/java/com/redhat/coolstore/cart/service/ShoppingCartServiceImplTest.java
+++ lab-02/src/test/java/com/redhat/coolstore/cart/service/ShoppingCartServiceImplTest.java
@@ -9,8 +9,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Optional;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
